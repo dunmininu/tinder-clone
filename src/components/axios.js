1eec3d5d01ac8221d0dec97cf25cef097d23cf71
dunmininu <oluwaseyi.alaka@torilo.co.uk>
2022-12-28 18:16:06 +0100

    live backend

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8001/api/main'
-    // baseURL: 'https://mynodebackend.onrender.com/api/main'
+    // baseURL: 'http://localhost:8001/api/main'
+    baseURL: 'https://mynodebackend.onrender.com/api/main'
 })
 
 export default instance;
